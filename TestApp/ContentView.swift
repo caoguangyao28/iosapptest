5ee392abbc44588d491aa4e02910f265b8c89024
曹光耀 <caoguangyao@chinasofti.com>
2021-02-22 21:57:29 +0800

    Initial Commit

---
+++ TestApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TestApp
+//
+//  Created by 曹光耀 on 2021/2/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
