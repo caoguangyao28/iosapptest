8441057c33aa4863a990bad0e0380b77c0ef388b
曹光耀 <caoguangyao@chinasofti.com>
2021-03-09 11:09:54 +0800

    feat: study one

--- TestApp/ContentView.swift
+++ TestApp/ContentView.swift
@@ -8,14 +8,24 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var alertIsVisible: Bool = false
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Text("2021年加油1")
+                .fontWeight(.semibold)
+                .foregroundColor(Color.green)
+            Button(action: {
+                print("这辈子都没见过这样的要求")
+                self.alertIsVisible = true //更改状态
+            }) {
+                Text("过来点我呀")
+            }
+            .alert(isPresented: self.$alertIsVisible){
+                                Alert(title: Text("你好"),
+                                      message: Text("这样的要求不算奇怪"),
+                                      dismissButton: .default(Text("太好了！")))
+                            }
+        }
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
-}
